8a0f1f945d77e296599f6a581b54e10026b76794
karlsainas1 <karl.sainas@khk.ee>
2018-10-11 11:30:01 +0300

    lisatud grammide arvutamise funktsioon
    Signed-off-by: karlsainas1 <karl.sainas@khk.ee>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <test />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import test from './components/konverter.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    test
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/konverter.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+   <h1>Konverter</h1>
+   <h4>sisesta kaal kilogrammides:</h4>
+    <input type="number" v-model="kg" min=0>
+    
+     <table>
+        <tr>
+        <td>Grammid</td>
+        <td> {{`${kg*1000}g`}} </td>
+    </tr>
+    <tr>
+        <td>Naelad</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Untsid</td>
+        <td></td>
+    </tr>
+        
+    </table>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'konverter',
+  data(){
+      return{
+          kg:0
+          
+      }
+  },
+    methods:{
+      
+        
+        
+        
+    },
+    computed: {
+        
+                
+        }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+
+<style scoped>
+
+</style>
